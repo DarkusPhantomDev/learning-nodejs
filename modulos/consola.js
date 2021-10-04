8d051be45bd77c2fed73d04e5cad021c68ed5123
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-10-04 12:49:59 -0400

    Aprendiendo acerca de la consola

---
+++ modulos/consola.js
@@ -0,0 +1,12 @@
+//console.log("dorime");
+//console.error("Error");
+//console.warn("Advertencia");
+//console.trace("Investigar esto");
+//console.table("Muestra tabla");
+//console.count("Contador");
+//console.countReset("Reinicia el contador")
+//console.time("Inicia cronometro en ms")
+//console.timeEnd("Finaliza el cronometro");
+//console.group("Perimte agrupar errores mediante identacion")
+//console.groupEnd()
+//console.clear("Limpia pantalla")
